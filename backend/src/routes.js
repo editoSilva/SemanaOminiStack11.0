2492db6377f7a57b618c2572f1cc837c84e000f0
Edito <editosilva>
2020-03-24 19:44:24 -0300

    Primeiro commit backend

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = express.Router();
+
+const SessionController = require('./controllers/SessionController');
+const ProfileController = require('./controllers/ProfileController');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+
+routes.post('/session', SessionController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+
+
+
+module.exports = routes;
